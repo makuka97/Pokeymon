1e64cd6eab89c7603524c2b714b5e52c0495b0d8
A Student <student@nowhere.edu>
2026-01-10 08:29:46 +0000

    Jan 10, 2026, 12:29 AM

--- Pokeymon.cpp
+++ Pokeymon.cpp
@@ -80,7 +80,7 @@ std::string attack(const Pokeymon& attacker, Pokeymon& defender) {
     }
 
     //Create and return the message
-    std:: message = attacker.name + "dealt" + std::to_string(damage) + " damage to " + defender.name + ". " + defender.name 
+    std:: message = attacker.name + "dealt" + std::to_string(damage) + " damage to " + defender.name + ". " + defender.name + " has " + std::to_string(defender)
 
 }
 
