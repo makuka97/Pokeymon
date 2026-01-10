f4c6a5d6d6c4ac55907397e2cb4600f95d07df27
A Student <student@nowhere.edu>
2026-01-10 08:00:38 +0000

    Jan 10, 2026, 12:00 AM

--- Pokeymon.cpp
+++ Pokeymon.cpp
@@ -49,7 +49,7 @@ bool isStrongAgainst(Type attacker, Type defender) {
 }
 
 std::string getDescription(const Pokeymon& p) {
-    std::string desc == "Name " + p.name + ", Type: " + typeToString(p.type) + ", Health: "  + to_string(p.currentHealth) + "/" + to_string(p.maxealth) + ", Atk: " + to_string(p.attack) + " Def " + to_string(p.defense);
+    std::string desc = "Name " + p.name + ", Type: " + typeToString(p.type) + ", Health: "  + to_string(p.currentHealth) + "/" + to_string(p.maxealth) + ", Atk: " + to_string(p.attack) + " Def " + to_string(p.defense);
     return desc;
 }
 
